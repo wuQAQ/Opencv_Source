f936a40decc2b815f0e9db9bad56f0625d49fb9f
wuQAQ <302538257@qq.com>
2017-04-19 01:58:32 +0800

    test

--- writemat/main.cpp
+++ writemat/main.cpp
@@ -1,31 +1,37 @@
 #include "opencv2/opencv.hpp"
-#include <time.h>
+#include <iostream>
+#include <vector>
 
+using namespace std;
 using namespace cv;
 
 int main(int, char** argv)
 {
     FileStorage fs("test.yml", FileStorage::WRITE);
-
-    fs << "frameCount" << 5;
-    time_t rawtime; time(&rawtime);
-    fs << "calibrationDate" << asctime(localtime(&rawtime));
-    Mat cameraMatrix = (Mat_<double>(3,3) << 1000, 0, 320, 0, 1000, 240, 0, 0, 1);
-    Mat distCoeffs = (Mat_<double>(5,1) << 0.1, 0.01, -0.001, 0, 0);
-    fs << "cameraMatrix" << cameraMatrix << "distCoeffs" << distCoeffs;
-    fs << "features" << "[";
-    for( int i = 0; i < 3; i++ )
+    vector<Mat> test;
+    Mat temp = Mat::zeros(3, 3, CV_64F);
+    for (int i = 0; i < 9; i++)
     {
-        int x = rand() % 640;
-        int y = rand() % 480;
-        uchar lbp = rand() % 256;
-
-        fs << "{:" << "x" << x << "y" << y << "lbp" << "[:";
-        for( int j = 0; j < 8; j++ )
-            fs << ((lbp >> j) & 1);
-        fs << "]" << "}";
+        Mat tem_frame = temp.clone();
+        test.push_back(temp.clone());
+        //test.at(0) = temp;
     }
-    fs << "]";
+    
+    cout << "temp: " << endl << temp << endl;
+
+    for(int i = 0; i < test.size(); i++)
+    {
+        cout << i << endl << test[i] << endl;
+    }
+
+    cout << "write" << endl;
+
+    fs << "a" << test.at(1);
+    fs << "b" << test.at(2);
+    
+
     fs.release();
+
+    
     return 0;
 }
